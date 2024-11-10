ddada48c7dd136020da8032ec785a68a287d4095
Pierre Lasante <pierre.lasante@videotron.ca>
2024-11-10 08:58:34 -0500

    Formik Hooks

--- src/components/formTwo.jsx
+++ src/components/formTwo.jsx
@@ -1,14 +1,33 @@
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
 const FormTwo = () => {
+
+    const formik = useFormik({
+        initialValues: {firstname:''},
+        validationSchema: Yup.object({
+            firstname: Yup.string().required("Please enter firstname"),
+        }),
+        onSubmit: (values) => {
+            console.log(values);
+        }
+    })
+
     return(
         <div className="container">
             <div className="col-md-12 mt-5">
-                <form>
+                <form onSubmit={formik.handleSubmit}>
                     <label htmlFor="firstname">First name</label>
                     <input 
                         className="form-control" 
                         type="text" 
                         name="firstname"
+                        {...formik.getFieldProps('firstname')}
                     />
+                    { formik.errors.firstname && formik.touched.firstname ?
+                        <span>{formik.errors.firstname}</span>
+                        :null
+                    }
                     
                     <hr className="mb-4" />
                     <button className="btn btn-primary btn-lg btn-block" type="submit">
