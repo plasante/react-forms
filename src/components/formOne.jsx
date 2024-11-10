ad2632f54be814b7a216b60041285c01d71d532f
Pierre Lasante <pierre.lasante@videotron.ca>
2024-11-10 08:12:29 -0500

    Manual validation

--- src/components/formOne.jsx
+++ src/components/formOne.jsx
@@ -4,14 +4,36 @@ const FormOne = () => {
 
   return (
       <Formik
-        initialValues={{
-          firstname: '',
-          lastname: '',
-          email: '',
-          country: '',
-          state: '',
-          zip: ''
-        }}
+          initialValues={{
+            firstname: '',
+            lastname: '',
+            email: '',
+            country: '',
+            state: '',
+            zip: ''
+          }}
+          validate={(values) => {
+            const errors = {};
+
+            if (!values.firstname) {
+              errors.firstname = 'First name is required';
+            }
+            if (!values.lastname) {
+              errors.lastname = 'Last name is required';
+            }
+            if (!values.email) {
+              errors.email = 'Email is required';
+            } else if(!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(values.email))
+            {
+              errors.email = 'Invalid email address';
+            }
+
+
+            return errors;
+          }}
+          onSubmit={(values) => {
+            console.log(values);
+          }}
       >
         {({
             values,
@@ -24,24 +46,54 @@ const FormOne = () => {
           }) => (
             <div className="container">
               <div className="col-md-12 mt-5">
-                <form>
+                <form onSubmit={handleSubmit}>
                   <h4 className="mb-3">Personal information</h4>
 
                   <div className="row">
                     <div className="col-md-6 mb-3">
                       <label htmlFor="firstname">First name</label>
-                      <input type="text" className="form-control" id="firstname" name="firstname"/>
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="firstname"
+                        onChange={handleChange}
+                        value={values.firstname}
+                      />
+                      { errors.firstname && touched.firstname ?
+                          <span>{errors.firstname}</span>
+                          : null
+                      }
                     </div>
                     <div className="col-md-6 mb-3">
                       <label htmlFor="lastname">Last name</label>
-                      <input type="text" className="form-control" id="lastname" name="lastname"/>
+                      <input
+                          type="text"
+                          className="form-control"
+                          id="lastname"
+                          onChange={handleChange}
+                          value={values.lastname}
+                      />
+                      { errors.lastname && touched.lastname ?
+                          <span>{errors.lastname}</span>
+                          : null
+                      }
                     </div>
                   </div>
 
                   <div className="mb-3">
                     <label htmlFor="email">Email</label>
-                    <input type="email" className="form-control" id="email" name="email"
-                           placeholder="you@example.com"/>
+                    <input
+                        type="email"
+                        className="form-control"
+                        id="email"
+                        onChange={handleChange}
+                        value={values.email}
+                        placeholder="you@example.com"
+                    />
+                    { errors.email && touched.email ?
+                        <span>{errors.email}</span>
+                        : null
+                    }
                   </div>
 
 
